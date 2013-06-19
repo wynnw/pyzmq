e363dd101d9f3802360955b4ac04a78cec770239
Wynn Wilkes <wynnwilkes@gmail.com>
2013-06-19 14:39:06 -0600

    change zmq version detection to use header macros.
    This allows us to build vers.c without linking to libzmq. Running vers can fail at times
    if the libzmq library has an install name of @executable_path/../ on darwin and we don't
    match the intended bundle layout during the pip build.
    (the same problem could potentially occur on linux with $ORIGIN)

--- buildutils/detect.py
+++ buildutils/detect.py
@@ -37,10 +37,10 @@ def test_compilation(cfile, compiler=None, **compiler_attrs):
             customize_mingw(cc)
     else:
         cc = compiler
-    
+
     for name, val in compiler_attrs.items():
         setattr(cc, name, val)
-    
+
     efile, ext = os.path.splitext(cfile)
 
     cpreargs = lpreargs = None
@@ -76,49 +76,57 @@ def compile_and_run(basedir, src, compiler=None, **compiler_attrs):
         se = se.decode()
     finally:
         shutil.rmtree(basedir)
-    
+
     return result.returncode, so, se
-    
-    
+
+
 def detect_zmq(basedir, compiler=None, **compiler_attrs):
     """Compile, link & execute a test program, in empty directory `basedir`.
-    
+
     The C compiler will be updated with any keywords given via setattr.
-    
+
     Parameters
     ----------
-    
+
     basedir : path
         The location where the test program will be compiled and run
     compiler : str
         The distutils compiler key (e.g. 'unix', 'msvc', or 'mingw32')
     **compiler_attrs : dict
         Any extra compiler attributes, which will be set via ``setattr(cc)``.
-    
+
     Returns
     -------
-    
+
     A dict of properties for zmq compilation, with the following two keys:
-    
+
     vers : tuple
         The ZMQ version as a tuple of ints, e.g. (2,2,0)
     settings : dict
         The compiler options used to compile the test function, e.g. `include_dirs`,
         `library_dirs`, `libs`, etc.
     """
-    
+
+    # strip out the linking to zmq, as vers.c gets the version just from the header
+    restore_zmq = False
+    if 'zmq' in compiler_attrs['libraries']:
+        restore_zmq = True
+        compiler_attrs['libraries'].remove('zmq')
+
     cfile = pjoin(basedir, 'vers.c')
     shutil.copy(pjoin(os.path.dirname(__file__), 'vers.c'), cfile)
-    
+
     # check if we need to link against Realtime Extensions library
     if sys.platform.startswith('linux'):
         cc = ccompiler.new_compiler(compiler=compiler)
         cc.output_dir = basedir
         if not cc.has_function('timer_create'):
             compiler_attrs['libraries'].append('rt')
-            
+
     efile = test_compilation(cfile, compiler=compiler, **compiler_attrs)
-    
+    if restore_zmq:
+        compiler_attrs['libraries'].append('zmq')
+
     result = Popen(efile, stdout=PIPE, stderr=PIPE)
     so, se = result.communicate()
     # for py3k:
--- buildutils/vers.c
+++ buildutils/vers.c
@@ -5,7 +5,11 @@
 
 int main(int argc, char **argv){
     int major, minor, patch;
-    zmq_version(&major, &minor, &patch);
+
+    major = ZMQ_VERSION_MAJOR;
+    minor = ZMQ_VERSION_MINOR;
+    patch = ZMQ_VERSION_PATCH;
+
     fprintf(stdout, "vers: %d.%d.%d\n", major, minor, patch);
     return 0;
 }
